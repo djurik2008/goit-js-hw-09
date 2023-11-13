8d1583fa3271622623fc1e200b1c8ce9a0b5f59b
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2023-11-13 17:04:19 +0000

    Deploying to gh-pages from @ djurik2008/goit-js-hw-09@04af2b1fe999239c6adbcb7720cd9eb1cdc6e8af 🚀

---
+++ 03-promises.8f9e7118.js
@@ -0,0 +1,2 @@
+document.querySelector(".form button"),document.querySelector("form");
+//# sourceMappingURL=03-promises.8f9e7118.js.map
---
+++ 03-promises.b7eff73f.js
@@ -0,0 +1,2 @@
+document.querySelector(".form button"),document.querySelector("form");
+//# sourceMappingURL=03-promises.b7eff73f.js.map
